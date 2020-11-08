3d77b93742415b37501ff28cb113f196e403466a
Dilshan Liyanage <>
2020-11-08 22:28:51 +0530

    grid items

--- src/app/angular-material-data-grid/angular-material-data-grid.component.ts
+++ src/app/angular-material-data-grid/angular-material-data-grid.component.ts
@@ -1,5 +1,13 @@
-import { AfterContentInit, ChangeDetectorRef, Component, HostListener, Renderer2 } from '@angular/core';
-import { environment } from '../../environments/environment';
+import {
+  AfterContentInit,
+  ChangeDetectorRef,
+  Component,
+  HostListener,
+  Input,
+  Output,
+  EventEmitter,
+  Renderer2
+} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatDialog } from '@angular/material/dialog';
 import { moveItemInArray } from '@angular/cdk/drag-drop';
@@ -12,50 +20,13 @@ import { ApiResponseModel } from './api-response.model';
 })
 export class AngularMaterialDataGridComponent implements AfterContentInit{
 
-  headings: any = [ {fieldName: 'uid', display: 'ID', type: 'number', minWidth: '160px', maxWidth: '160px', width: '12.25%', filter: true,
-    disableSorting: true},
-    {fieldName: 'first_name', display: 'First Name', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%'},
-    {fieldName: 'email', display: 'Email', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%'},
-    {fieldName: 'gender', display: 'Gender', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%',
-      filterType: 'multi-select',
-      other: {
-        selectionMode: 'single',
-        source: 'internal',
-        url: 'gender',
-        optionsObject: [
-          {text : 'Female', value: 'FEMALE'},
-          {text : 'Male', value: 'MALE'}
-        ],
-        stringList: true
-      }
-    },
-    {fieldName: 'date_of_birth', display: 'Date Of Birth', type: 'date', minWidth: '160px', maxWidth: '160px', width: '12.25%'},
-    {fieldName: 'ip_address', display: 'IP Address', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%'},
-    {fieldName: 'car', display: 'Car', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%'},
-    {fieldName: 'phone', display: 'Phone', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%'},
-    {fieldName: 'movie_taste', display: 'Movie Taste', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
-    {fieldName: 'country', display: 'Country', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%',
-      filterType: 'multi-select',  show: true,
-      other: {
-        selectionMode: 'multiple',
-        source: 'external',
-        url: `${environment.api}countries`,
-        key: 'displayName',
-        value: 'value',
-        stringList: true
-      }
-    },
-    {fieldName: 'city', display: 'City', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
-    {fieldName: 'company', display: 'Company', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
-    {fieldName: 'iban', display: 'Iban', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
-    {fieldName: 'latitude', display: 'Latitude', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
-    {fieldName: 'longitude', display: 'Longitude', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
-    {fieldName: 'currency', display: 'Currency', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
-    {fieldName: 'username', display: 'Username', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
-    {fieldName: 'profession', display: 'Profession', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
-    {fieldName: 'state', display: 'State', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
-  ];
-  selection = true;
+  @Output() responseEmit: any = new EventEmitter<any>();
+  @Output() selectionEmit: any = new EventEmitter<any>();
+  @Output() filtersChangedEmit: any = new EventEmitter<any>();
+
+  @Input() headings: any = [];
+  @Input() selection = true;
+  @Input() url = '';
   allGridItemsSelected = false;
   loadingData = true;
   response = { gridData: [], totalCount: 0};
@@ -80,7 +51,7 @@ export class AngularMaterialDataGridComponent implements AfterContentInit{
 
   // Window resize listener
   @HostListener('window:resize', ['$event'])
-  onResize(event): void {
+  onResize(): void {
     this.calculateGridWidth();
   }
 
@@ -175,26 +146,21 @@ export class AngularMaterialDataGridComponent implements AfterContentInit{
     } else {
       this.filters.push(ev);
     }
+    this.filtersChangedEmit.emit(this.filters);
     this.pageChanged({pageNo: 1, recordsPerPage: this.recordsPerPage});
   }
 
-  allGridItemsSelectionChanged(): void {
+  gridItemSelectionChanged(all = false): void {
     this.selectedRows = [];
     this.response.gridData.forEach(item => {
-      item.gridItemSelected = this.allGridItemsSelected;
+      if (all) {
+        item.gridItemSelected = this.allGridItemsSelected;
+      }
       if (item.gridItemSelected) {
         this.selectedRows.push(item);
       }
     });
-  }
-
-  gridItemSelectionChanged(): void {
-    this.selectedRows = [];
-    this.response.gridData.forEach(item => {
-      if (item.gridItemSelected) {
-        this.selectedRows.push(item);
-      }
-    });
+    this.selectionEmit.emit(this.selectedRows);
   }
 
   // Multiple Selection logic
@@ -236,7 +202,7 @@ export class AngularMaterialDataGridComponent implements AfterContentInit{
         this.allCheckBoxesSelected = false;
       }
     }
-    // this.selectionEmit.emit(this.selectedRows);
+    this.selectionEmit.emit(this.selectedRows);
     // this.selectAllState = false;
   }
 
@@ -245,7 +211,7 @@ export class AngularMaterialDataGridComponent implements AfterContentInit{
     this.recordsPerPage = recordsPerPage;
     this.loadingData = true;
     this.currentPage = pageNo;
-    const url = `${environment.api}getUsers`;
+
     // const url = './assets/data.json';
     const body = {
       entity: {},
@@ -256,11 +222,12 @@ export class AngularMaterialDataGridComponent implements AfterContentInit{
     };
     console.log(body);
 
-    this.http.post<ApiResponseModel>(url, body).subscribe(data => {
+    this.http.post<ApiResponseModel>(this.url, body).subscribe(data => {
       // this.http.get<ApiResponseModel>(url).subscribe(data => {
       this.selectedRows = [];
       this.response = data.payload;
       console.log(this.response.gridData);
+      this.responseEmit.emit(this.response);
       this.loadingData = false;
       this.changeDetectorRef.detectChanges();
       document.getElementById('scrollViewport').scrollTop = 0;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  url = `${environment.api}getUsers`;
+  headings = [
+    {fieldName: 'uid', display: 'ID', type: 'number', minWidth: '160px', maxWidth: '160px', width: '12.25%', filter: true,
+      disableSorting: true},
+    {fieldName: 'first_name', display: 'First Name', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%'},
+    {fieldName: 'email', display: 'Email', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%'},
+    {fieldName: 'gender', display: 'Gender', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%',
+      filterType: 'multi-select',
+      other: {
+        selectionMode: 'single',
+        source: 'internal',
+        url: 'gender',
+        optionsObject: [
+          {text : 'Female', value: 'FEMALE'},
+          {text : 'Male', value: 'MALE'}
+        ],
+        stringList: true
+      }
+    },
+    {fieldName: 'date_of_birth', display: 'Date Of Birth', type: 'date', minWidth: '160px', maxWidth: '160px', width: '12.25%'},
+    {fieldName: 'ip_address', display: 'IP Address', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%'},
+    {fieldName: 'car', display: 'Car', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%'},
+    {fieldName: 'phone', display: 'Phone', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%'},
+    {fieldName: 'movie_taste', display: 'Movie Taste', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
+    {fieldName: 'country', display: 'Country', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%',
+      filterType: 'multi-select',  show: true,
+      other: {
+        selectionMode: 'multiple',
+        source: 'external',
+        url: `${environment.api}countries`,
+        key: 'displayName',
+        value: 'value',
+        stringList: true
+      }
+    },
+    {fieldName: 'city', display: 'City', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
+    {fieldName: 'company', display: 'Company', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
+    {fieldName: 'iban', display: 'Iban', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
+    {fieldName: 'latitude', display: 'Latitude', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
+    {fieldName: 'longitude', display: 'Longitude', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
+    {fieldName: 'currency', display: 'Currency', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
+    {fieldName: 'username', display: 'Username', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
+    {fieldName: 'profession', display: 'Profession', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
+    {fieldName: 'state', display: 'State', type: 'string', minWidth: '160px', maxWidth: '160px', width: '12.25%', show: false},
+  ];
 
+  responseReceived(data): void {
+    console.log(data);
+  }
+
+  selectionChanged(data): void {
+    console.log(data);
+  }
+
+  filtersChanged(data): void {
+    console.log(data);
+  }
 }

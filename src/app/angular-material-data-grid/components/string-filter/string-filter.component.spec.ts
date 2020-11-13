a175db7fd265eeb85ea93459e8ae4b794d33bb95
Dilshan Liyanage <>
2020-11-14 03:06:30 +0530

    update angular version

--- src/app/angular-material-data-grid/components/string-filter/string-filter.component.spec.ts
+++ src/app/angular-material-data-grid/components/string-filter/string-filter.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { StringFilterComponent } from './string-filter.component';
 
@@ -6,7 +6,7 @@ describe('StringFilterComponent', () => {
   let component: StringFilterComponent;
   let fixture: ComponentFixture<StringFilterComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ StringFilterComponent ]
     })

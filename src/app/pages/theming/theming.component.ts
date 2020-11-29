a8cd2bdc083aed430e49676e05e63f46da5bb092
Dilshan Liyanage <nelly.dil@hotmail.com>
2020-11-30 01:38:51 +0530

    wording changes

--- src/app/pages/theming/theming.component.ts
+++ src/app/pages/theming/theming.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThemingComponent implements OnInit {
 
+  ts = `
+  import { Renderer2 } from '@angular/core';
+  ...
+  export class SampleComponent {
+    darkMode = true;
+    constructor(private renderer: Renderer2) {
+      if (this.darkMode) {
+        this.renderer.addClass(document.body, 'darkMode'); // add this
+      }
+    }
+  }
+`;
   constructor() { }
 
   ngOnInit(): void {
